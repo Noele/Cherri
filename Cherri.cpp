3c1aca849ee6b78318176d1317175a65f0b9944b
Noele <40408923+Noele@users.noreply.github.com>
2020-12-13 18:21:05 +0000

    Documentation added

--- Cherri.cpp
+++ Cherri.cpp
@@ -1,32 +1,39 @@
 #include "Cherri.h"
-Command* Cherri::commands[] = {new Avatar("avatar"), new Daily("daily"), new Balance("balance")};
+Command* Cherri::commands[] = {new Avatar("avatar"), new Daily("daily"), new Balance("balance")}; // Hold instances of our commands in an array
 void Cherri::onMessage(SleepyDiscord::Message message) {
+    // Split the message at spaces
     std::istringstream iss(message.content);
     std::vector<std::string> results(std::istream_iterator<std::string>{iss}, std::istream_iterator<std::string>());
+
+    // Instantiate for scope
     Response response;
     SleepyDiscord::User contextUser;
     
+    // If a user has been mentioned, set them as the context user, else set it as the message author
     try { 
         contextUser = results.size() == 1 ? message.author : getUser(Toolbox::regexRemove(results[1], "[^0-9]+"));
     } catch (const std::exception& e) {
         contextUser = message.author;
     }
     
+    // If the message starts with our prefix + one of our commands names, execute it and retrive its response
     for(Command* c : Cherri::commands) {
         if (message.startsWith(Config::prefix + c->name)) {
             response = c->execute(message, contextUser);
         }
     }
     
+    // Switch on response type
     switch(response.rtype) {
-        case Response::type::embed: 
-            sendMessage(message.channelID, response.rmessage, response.rembed);
+        case Response::type::embed: // If the response type is embed
+            sendMessage(message.channelID, response.rmessage, response.rembed); // Send a blank message* along with the response embed
             break;
-        case Response::type::message: 
-            sendMessage(message.channelID, response.rmessage);
+        case Response::type::message: // If the response type is message
+            sendMessage(message.channelID, response.rmessage); // Send the response message
             break;
-        case Response::type::action:
-            break;
+        case Response::type::action: // If the response type is action
+            break; // Yet to be implemented
     }
+} 
 
-}  
+// * Due to a bug, a "-" will be sent along side the embed
--- Toolbox.cpp
+++ Toolbox.cpp
@@ -1,9 +1,20 @@
 #include "Toolbox.h"
 
+/**
+ * Gets the correct extension of a users avatar
+ * @param userAvatar The hash of a given users avatar
+ * 
+ * @note returns .gif if userAvatar starts with a_, else .png
+ */
 std::string Toolbox::returnExtention(std::string userAvatar) {
     return std::string(userAvatar.rfind("a_", 0) == 0 ? ".gif" : ".png");
 }
 
+/**
+ * Removes text from a message using the given regex
+ * @param string The string we should remove text from
+ * @param regex The regex to validate the removal from string
+ */
 std::string Toolbox::regexRemove(std::string string, std::string regex) {
     std::regex e (regex);
     return std::regex_replace(string, e, "");
